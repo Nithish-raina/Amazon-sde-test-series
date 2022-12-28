6571da393260e0094c822d50c21b0007115127c6
Nithish-maker <nithishraina0323@gmail.com>
2022-12-28 21:53:20 +0530

    added readability

--- array/demo.js
+++ array/demo.js
@@ -1,9 +1,12 @@
-/** Adds an image with a random link */
+/** Adds an image with a random link in an object literal */
 const urls = ["https://link-1.com/", "https://link-2.com/", 
 "https://link-3.com/", "https://link-4.com/", "https://link-5.com/"];
 const person = {
   id:1,
   name:"user name",
+  gender:"male",
+  isEmployed:"booleanValue",
+  enrollmentNo: "513418104025",
   age:22,
 };
 const rId = Math.round(Math.random() * urls.length - 1);
