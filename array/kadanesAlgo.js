e77489c8dc312a62bc3d2776aeb48edef9fb29ee
Nithish-maker <nithishraina0323@gmail.com>
2023-01-12 12:54:42 +0530

    kadanes algo for maxSum of a subarr

---
+++ array/kadanesAlgo.js
@@ -0,0 +1,14 @@
+/*  Returns the maximum sum of a subarray containing integers from (-infinity, infinity)
+ *  @param {number[]} arr Input array
+ *  @return {number} maxSum Maximum sum of a subarray
+*/
+((arr) => {
+  let curSum = arr[0], maxSum = arr[0];
+  for(let i = 1; i < arr.length; i++) {
+    curSum = (curSum < 0) ? arr[i] : curSum + arr[i];
+    maxSum = Math.max(maxSum, curSum);
+    console.log(maxSum);
+    return maxSum;
+  }
+})([-1, -2, 3, -2, 5]);
+
