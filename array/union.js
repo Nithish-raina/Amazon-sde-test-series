beadcc610988fefc439c835a925283404de56bec
Nithish-maker <nithishraina0323@gmail.com>
2023-01-17 21:59:27 +0530

    union of 2 sorted arrays added

---
+++ array/union.js
@@ -0,0 +1,32 @@
+const union = (arr1, arr2, n, m) => {
+  let uni = [];
+  let i = 0, j = 0;
+  while(i < n && j < m) {
+    if(arr1[i] < arr2[j]) {
+      uni.push(arr1[i++]);
+      // Pointing i to a next distinct element to avoid duplicates
+      while(i < n && arr1[i] == arr1[i - 1]) i++;
+    }
+    else if(arr1[i] > arr2[j]) {
+      uni.push(arr2[j++]);
+      while(j < m && arr2[j] == arr2[j - 1]) j++;
+    }
+    else {
+      uni.push(arr1[i++]);
+      j++;
+      while(i < n && arr1[i] == arr1[i - 1]) i++;
+      while(j < m && arr2[j] == arr2[j - 1]) j++;
+    }
+  }
+  while(i < n) {
+    uni.push(arr1[i++]);
+    while(i < n && arr1[i] == arr1[i - 1]) i++;
+  }
+  while(j < m) {
+    uni.push(arr2[j++]);
+    while(j < m && arr2[j] == arr2[j - 1]) j++;
+  }
+  return uni; 
+}
+
+console.log(union([2, 2, 3, 4, 5], [1, 1, 2, 3, 4], 5, 5));
